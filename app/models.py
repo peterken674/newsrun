72bdc704cea9ad884ff4675e5c36098b538c64b4
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-06-05 22:48:37 +0300

    Add sources class and design UI for sources.

---
+++ app/models.py
@@ -0,0 +1,9 @@
+class Source:
+    '''
+    Source class to define sources.
+    '''
+    def __init__(self, src_id, src_name, src_category, src_url):
+        self.src_id = src_id
+        self.src_name = src_name
+        self.src_category = src_category
+        self.src_url = src_url
