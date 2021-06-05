840cc48088401d4f7320e3ffff9bd66143a1d694
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-06-06 00:14:23 +0300

    Successful sources request.

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, url_for, redirect
 from . import main
+from ..requests import get_sources
 
 
 @main.route('/')
@@ -7,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    sources = get_sources()
+    return render_template('index.html', sources = sources)
--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    NEWS_SRC_API_BASE_URL = 'https://newsapi.org/v2/sources?apiKey={}&language=en'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
